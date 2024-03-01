d2c326eb67d51645bba0759cc43c06b81eaf08c3
Pedro Araujo Dearmas <115725683+PedroDearmas@users.noreply.github.com>
2024-03-01 20:28:43 -0300

    Aula 01/03

--- Modal2/App.js
+++ Modal2/App.js
@@ -1,24 +1,98 @@
 import React, { useState } from "react";
 import Modal from "react-native-modal";
 import { Button } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 function Modal2() {
   const [isModalVisible, setModalVisible] = useState(false);
+  const [isModalVisibleSucesso, setModalVisibleSucesso] = useState(false);
+  const [isModalVisibleAtencao, setModalVisibleAtencao] = useState(false);
+  const [isModalVisibleAdvertencia, setModalVisibleAdvertencia] =
+    useState(false);
 
   const switchModal = () => {
     setModalVisible(!isModalVisible);
   };
+  const switchModalSucesso = () => {
+    setModalVisibleSucesso(!isModalVisibleSucesso);
+  };
+  const switchModalAtencao = () => {
+    setModalVisibleAtencao(!isModalVisibleAtencao);
+  };
+  const switchModalAdvertencia = () => {
+    setModalVisibleAdvertencia(!isModalVisibleAdvertencia);
+  };
+
   return (
     <View style={styles.container}>
-      <Button title="Clique Aqui" onPress={switchModal} />
+      <Button title="Ninja Go" onPress={switchModal} />
       <Modal
         isVisible={isModalVisible}
-        animationIn={"zoomInUp"}
-        animationInTiming={2900}
+        animationIn={"fadeInRight"}
+        animationInTiming={1000}
+        animationOut={"fadeOutLeft"}
+        animationOutTiming={1000}
       >
         <View style={styles.modalView}>
-          <Button title="Clique Aqui Para Voltar" onPress={switchModal} />
+          <Image
+            source={require("./assets/ninja.gif")}
+            style={{ width: 180, height: 180 }}
+          />
+          <Button title="Good Bye Ninja" onPress={switchModal} />
+        </View>
+      </Modal>
+
+      <Button title="Sucesso" onPress={switchModalSucesso} />
+
+      <Modal
+        isVisible={isModalVisibleSucesso}
+        animationIn={"zoomIn"}
+        animationInTiming={1000}
+        animationOut={"zoomOut"}
+        animationOutTiming={1000}
+      >
+        <View style={styles.modalView}>
+          <Image
+            source={require("./assets/mark2.gif")}
+            style={{ width: 180, height: 180 }}
+          />
+          <Button title="Voltar" onPress={switchModalSucesso} />
+        </View>
+      </Modal>
+
+      <Button title="Atencao" onPress={switchModalAtencao} />
+
+      <Modal
+        isVisible={isModalVisibleAtencao}
+        animationIn={"zoomIn"}
+        animationInTiming={1000}
+        animationOut={"zoomOut"}
+        animationOutTiming={1000}
+      >
+        <View style={styles.modalView}>
+          <Image
+            source={require("./assets/atencao.gif")}
+            style={{ width: 180, height: 180 }}
+          />
+          <Button title="Voltar" onPress={switchModalAtencao} />
+        </View>
+      </Modal>
+
+      <Button title="Advertencia" onPress={switchModalAdvertencia} />
+
+      <Modal
+        isVisible={isModalVisibleAdvertencia}
+        animationIn={"zoomIn"}
+        animationInTiming={1000}
+        animationOut={"zoomOut"}
+        animationOutTiming={1000}
+      >
+        <View style={styles.modalView}>
+          <Image
+            source={require("./assets/x.gif")}
+            style={{ width: 180, height: 180 }}
+          />
+          <Button title="Voltar" onPress={switchModalAdvertencia} />
         </View>
       </Modal>
     </View>
@@ -29,6 +103,7 @@ export default Modal2;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 10,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -40,9 +115,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "lightblue",
-    margin: 80,
-    marginTop: 150,
-    marginBottom: 150,
+    margin: 50,
+    marginTop: 120,
+    marginBottom: 120,
     borderRadius: 50,
   },
 });
